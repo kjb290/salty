c7fbe5e4b559eb24b9de987a45e7a03c41b4cfe7
kjb290 <kory.j.blose@gmail.com>
2018-08-09 15:09:10 -0400

    first commit

---
+++ kg_salt.py
@@ -0,0 +1,213 @@
+import matplotlib.pyplot as plt
+import keras
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from PIL import Image
+import os
+import sys
+import random
+import warnings
+
+import math
+
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from itertools import chain
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import tensorflow as tf
+
+
+ids = ['0fea4b5049', '0a41de5e3b', '0aabdb423e']
+plt.figure(figsize=(20, 10))
+for j, img_name in enumerate(ids):
+    q = j + 1
+    img = load_img('input/train/images/' + img_name + '.png')
+    img_mask = load_img('input/train/masks/' + img_name + '.png')
+
+    plt.subplot(1, 2 * (1 + len(ids)), q * 2 - 1)
+    plt.imshow(img)
+    plt.subplot(1, 2 * (1 + len(ids)), q * 2)
+    plt.imshow(img_mask)
+plt.show()
+
+
+im_width = 128
+im_height = 128
+im_chan = 3
+path_train = 'input/train'
+path_test = 'test'
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+seed = 42
+random.seed = seed
+np.random.seed = seed
+
+train_ids = next(os.walk('input/train/images'))[2]
+mask_ids = next(os.walk('input/train/masks'))[2]
+test_ids = next(os.walk('test/images'))[2]
+
+# Get and resize train images and masks
+X_train = np.zeros((len(mask_ids), im_height, im_width, im_chan), dtype=np.uint8)
+Y_train = np.zeros((len(mask_ids), im_height, im_width, 1), dtype=np.bool)
+print('Getting and resizing train images and masks ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(mask_ids), total=len(mask_ids)):
+    img = load_img(path_train + '/images/' + id_)
+    x = img_to_array(img)[:,:,1]
+    x = resize(x, (128, 128, 1), mode='constant', preserve_range=True)
+    X_train[n] = x
+    mask = img_to_array(load_img(path_train + '/masks/' + id_))[:,:,1]
+    Y_train[n] = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
+
+print('Done!')
+
+# Check if training data looks all right
+ix = random.randint(0, len(mask_ids))
+plt.imshow(np.dstack((X_train[ix,:,:,0],X_train[ix,:,:,1],X_train[ix,:,:,2])))
+plt.show()
+tmp = np.squeeze(Y_train[ix]).astype(np.float32)
+plt.imshow(np.dstack((tmp,tmp,tmp)))
+plt.show()
+
+
+# Build U-Net model
+inputs = Input((im_height, im_width, im_chan))
+s = Lambda(lambda x: (x*x*x) / (255*255*255)) (inputs)
+
+c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (s)
+c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
+p1 = MaxPooling2D((2, 2)) (c1)
+
+c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
+c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
+p2 = MaxPooling2D((2, 2)) (c2)
+
+c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
+c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
+p3 = MaxPooling2D((2, 2)) (c3)
+
+c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
+c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
+p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
+c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
+
+u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = concatenate([u6, c4])
+c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
+c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+
+u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = concatenate([u7, c3])
+c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
+c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+
+u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = concatenate([u8, c2])
+c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
+c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+
+u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = concatenate([u9, c1], axis=3)
+c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
+c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+
+outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+model = Model(inputs=[inputs], outputs=[outputs])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+model.summary()
+
+earlystopper = EarlyStopping(patience=5, verbose=1)
+checkpointer = ModelCheckpoint('model-tgs-salt-1.h5', verbose=1, save_best_only=True)
+results = model.fit(X_train, Y_train, validation_split=0.3, batch_size=4, epochs=30,
+                    callbacks=[earlystopper, checkpointer])
+
+# Define IoU metric
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+# src: https://www.kaggle.com/aglotero/another-iou-metric
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    labels = y_true_in
+    y_pred = y_pred_in
+
+    true_objects = 2
+    pred_objects = 2
+
+    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins=true_objects)[0]
+    area_pred = np.histogram(y_pred, bins=pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:, 1:]
+    union = union[1:, 1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+
+    # Precision helper function
+    def precision_at(threshold, iou):
+        matches = iou > threshold
+        true_positives = np.sum(matches, axis=1) == 1   # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+
+    # Loop over IoU thresholds
+    prec = []
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        prec.append(p)
+
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = y_true_in.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        value = iou_metric(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    return np.mean(metric)
